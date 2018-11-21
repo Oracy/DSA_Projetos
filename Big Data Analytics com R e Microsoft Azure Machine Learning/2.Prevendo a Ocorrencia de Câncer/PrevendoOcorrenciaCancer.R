7f13f28d760b9e0fc2c700153a384214d1f6c3eb
Oracy <oramartos_21@hotmail.com>
2018-11-21 19:13:59 -0200

    Folder deleted

--- Big Data Analytics com R e Microsoft Azure Machine Learning/2.Prevendo a Ocorrencia de Câncer/PrevendoOcorrenciaCancer.R
+++
@@ -1,8 +0,0 @@
-# Set Directory
-setwd("/home/oracy/Documents/DSA_Projetos/DSA_Projetos/Big Data Analytics com R e Microsoft Azure Machine Learning/2.Prevendo a Ocorrencia de Câncer/")
-getwd()
-
-# Data Gathering
-df <- read.csv(file = "bc_data.csv", header = TRUE, sep = ",")
-
-str(df)
