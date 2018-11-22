02f4b22f7ecbd3e1469878aab71a92c668497657
Oracy <oramartos@msn.com>
2018-11-22 11:36:32 -0200

    Preventing Hospital Expenses missing PDF

--- Big Data Analytics com R e Microsoft Azure Machine Learning/3.Prevendo Despesas Hospitalares/PrevendoDespesasHospitalares.r
+++ Big Data Analytics com R e Microsoft Azure Machine Learning/3.Prevendo Despesas Hospitalares/PrevendoDespesasHospitalares.r
@@ -1,7 +1,7 @@
 # Preventing Hospital Expenses
 
 # Set Directory
-setwd("/home/oracy/Documents/DSA_Projetos/DSA_Projetos/Big Data Analytics com R e Microsoft Azure Machine Learning/3.Prevendo Despesas Hospitalares")
+setwd("C:\\Users\\omartos\\Git\\DSA_Projetos\\DSA_Projetos\\Big Data Analytics com R e Microsoft Azure Machine Learning\\3.Prevendo Despesas Hospitalares")
 getwd()
 
 # For this analysis, we will use a data set simulating hypothetical medical expenses
@@ -36,27 +36,58 @@ cor(df[c("idade","bmi", "filhos", "gastos")])
 
 # Viewing relationship between variables: Scatterplot
 # Note that there is no clear relationship between the variables
+pairs(df[c("idade", "bmi", "filhos", "gastos")])
 
 # Scatterplot Matrix
-# install.packages ("psych")
+# Font: http://www.sthda.com/english/wiki/scatter-plot-matrices-r-base-graphs#use-the-r-package-psych
+#install.packages ("psych")
+library(psych)
+pairs.panels(df[c("idade", "bmi", "filhos", "gastos")], method = "pearson", # correlation method
+             density = TRUE, # show density plots
+             ellipses = TRUE # show correlation ellipses
+             )
 
 # This graphic provides more information about the relationship between variables
 
-# Step 3: Training the Model
-
+## Step 3: Training the Model
+# Font: https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/lm
+str(df)
+model <- lm(gastos ~ idade + filhos + bmi + sexo + fumante + regiao, df)
+  
 # Similar to the previous item
+model_2 <- lm(gastos ~ ., df) # "." is the same as type all variables
 
 # Viewing the coefficients
+# Font: https://stackoverflow.com/questions/6577058/extract-regression-coefficient-values
+model_summary <- summary(model)
+model_summary
+model
 
 # Preventing medical expenses
+# Font: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/predict.lm.html
+predicting <- predict(model)
+class(predicting)
+head(predicting)
 
 # Step 4: Evaluating Model Performance
 # More details about the model
+# Font: https://stat.ethz.ch/R-manual/R-devel/library/stats/html/lm.html
+summary(model)
 
 # Step 5: Optimizing Model Performance
 
 # Adding a variable with twice the age value
+df$idade2 <- df$idade ^ 2
+df$idade2
+df$idade
 
 # Adding a Bookmark to BMI> = 30
+# Font: https://www.datamentor.io/r-programming/ifelse-function/
+df$bmi30 <- ifelse(df$bmi >= 30, 1, 0)
+df$bmi30
 
 # Creating the final template
+str(df)
+model_3 <- lm(gastos ~ idade + idade2 + sexo + filhos + bmi30 * fumante + regiao, df)
+
+summary(model_3)
